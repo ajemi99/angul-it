30f1f508eb654e2cd002e663e443aeebaaf4a8b0
ajemi <abdelmounaim.jemi@ump.ac.ma>
2026-06-29 18:22:28 +0100

    add home page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-  export class Home {
-    title = 'Angul-It';
+export class Home {
 
-    constructor(private router: Router) {}
+  title = 'Angul-It';
 
-    startChallenge(): void {
-      this.router.navigate(['/captcha']);
-    }
+  constructor(private router: Router) {}
+
+  startChallenge(): void {
+    this.router.navigate(['/captcha']);
   }
+
+}
